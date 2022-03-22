0f5df61986f7e05ecb9cd6fad4f047292dcf7aae
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-22 22:14:10 +0300

    pictures can now be changed

--- app/src/main/java/com/kotlincolorguessinggame/GameActivity.kt
+++ app/src/main/java/com/kotlincolorguessinggame/GameActivity.kt
@@ -2,10 +2,26 @@ package com.kotlincolorguessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.kotlincolorguessinggame.data.DataSource
+import com.kotlincolorguessinggame.databinding.ActivityGameBinding
+import com.kotlincolorguessinggame.databinding.ActivityMainBinding
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGameBinding
+    private var s = 1;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun answerButton(view: View) {
+        binding.flowerImage.setImageResource(DataSource().loadFlowers()[s])
+        s +=1
+        if(s == 10) s = 0
     }
 }
