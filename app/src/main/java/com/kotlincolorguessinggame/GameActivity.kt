2eb992402da4990b24f445a7a06b0fc1bb1d30d0
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-23 14:22:09 +0300

    questions can now be answered

--- app/src/main/java/com/kotlincolorguessinggame/GameActivity.kt
+++ app/src/main/java/com/kotlincolorguessinggame/GameActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import com.kotlincolorguessinggame.data.DataSource
 import com.kotlincolorguessinggame.databinding.ActivityGameBinding
 import com.kotlincolorguessinggame.databinding.ActivityMainBinding
@@ -11,7 +12,7 @@ import com.kotlincolorguessinggame.databinding.ActivityMainBinding
 class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameBinding
-    private var s = 1;
+    private var s = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +25,21 @@ class GameActivity : AppCompatActivity() {
     }
 
     fun answerButton(view: View) {
-        binding.flowerImage.setImageResource(DataSource().loadFlowers()[s])
-        binding.answer1.text = resources.getStringArray(DataSource().loadAnswerOptions()[s])[0]
-        binding.answer2.text = resources.getStringArray(DataSource().loadAnswerOptions()[s])[1]
-        binding.answer3.text = resources.getStringArray(DataSource().loadAnswerOptions()[s])[2]
-        s +=1
-        if(s == 10) s = 0
+        val userAnswer = findViewById<TextView>(view.id).text.toString()
+        Log.e("q", s.toString())
+
+        if (userAnswer == resources.getString(DataSource().loadAnswers()[s])) {
+            Log.e("q", "correct!")
+            s += 1
+            if (s == 10) s = 0
+            binding.flowerImage.setImageResource(DataSource().loadFlowers()[s])
+            binding.answer1.text = resources.getStringArray(DataSource().loadAnswerOptions()[s])[0]
+            binding.answer2.text = resources.getStringArray(DataSource().loadAnswerOptions()[s])[1]
+            binding.answer3.text = resources.getStringArray(DataSource().loadAnswerOptions()[s])[2]
+        } else {
+            Log.e("q", "incorrect!")
+        }
+
+
     }
 }
